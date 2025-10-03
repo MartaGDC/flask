2b15abb67256a999b649e7814bb6a2056b657c80
MartaGDC <gdc.marta@gmail.com>
2025-10-03 16:48:32 +0200

    nombres app tejidos

--- estructuraGral/app.py
+++ estructuraGral/app.py
@@ -11,7 +11,7 @@ app.secret_key = "secret_key"
 # CORS(app)
 BASE_DIR = "/srv/data" #cambiar acceso del usuario ubuntu para que sea como admin (chown y chmod 755)
 APP_VIDEOS = {
-    "tejidos":"0_",
+    "base_tejidos":"0_",
     
     "foot_longitudinal_fascia": "1_1_",
     "foot_transversal_fascia": "1_2_",
